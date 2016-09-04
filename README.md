207eefb0b4209667dccc289fdb63d8e5c635330a
ShinCurry <shincurryyang@gmail.com>
2016-09-04 19:21:05 +0800

    New feature - recognize black and white image. Fix bugs.

--- README.md
+++ README.md
@@ -5,8 +5,17 @@ show manga image without white background.
 
 ```javascript
 new MangaImage({
-    el: document.getElementsByTagName('article')[0],
-    scale: 0.85 // recommend 0.8 ~ 0.95
+    el: document.getElementsByTagName('article')[0], // section filter.
+    //imgClass: 'manga-image', //class filter.
+    auto: true, // auto recognize black and white image.
+    scale: 0.85 // recommend 0.8 ~ 0.95.
 });
 ```
 
+External image links must support CORS.
+
+Not support IE.
+
+## LICENSE
+
+This project is published under MIT License. See the LICENSE file for more.
--- manga-image.js
+++ manga-image.js
@@ -1,7 +1,9 @@
-MangaImage.prototype.transparent = function(img, scale = 0.85) {
-    img.crossOrigin = "Anonymous";
+MangaImage.prototype.transparent = function(img, options, callback) {
+    if (options.scale) options.scale = 0.85
+    img.crossOrigin = '';
     img.src = img.src;
     img.onload = function() {
+        img.onload = null;
         var canvas = document.createElement('canvas');
         var width = img.naturalWidth, height = img.naturalHeight;
         canvas.width = width;
@@ -10,40 +12,80 @@ MangaImage.prototype.transparent = function(img, scale = 0.85) {
         var ctx = canvas.getContext('2d');
         ctx.width = width;
         ctx.height = height;
-        ctx.drawImage(this, 0, 0, width, height);
+        ctx.drawImage(img, 0, 0, width, height);
 
         var imageData = ctx.getImageData(0, 0, width, height);
         var pixel = imageData.data;
         var r = 0, g = 1, b = 2, a = 3;
-        scale = scale * 255;
-        for (var p = 0; p< pixel.length; p += 4) {
-            if (scale <= pixel[p+r] &&
-                scale <= pixel[p+g] &&
-                scale <= pixel[p+b]) {
-                var total = pixel[p+r] + pixel[p+g] + pixel[p+b]
-                pixel[p+a] =  (total - 210*3) / total;
+        options.scale *= 255;
+
+        var gray = 0, color = 0;
+        for (var p = 0; p < pixel.length; p += 4) {
+            if (options.auto) {
+                //0-510
+                var value = (Math.abs(pixel[p+r] - pixel[p+g]) +
+                Math.abs(pixel[p+r] - pixel[p+b]) +
+                Math.abs(pixel[p+g] - pixel[p+b]));
+                if (value < 10) {
+                    gray++;
+                } else {
+                    color++;
+                }
+            }
+            if (options.scale <= pixel[p+r] &&
+                options.scale <= pixel[p+g] &&
+                options.scale <= pixel[p+b]) {
+                pixel[p+a] = 0;
             }
         }
+        if (options.auto) {
+            var ratio = gray / (gray + color)
+            if (ratio < 0.2) {
+                // console.log(this.src +' is full color img');
+                return;
+            } else if (ratio < 0.5) {
+                // console.log(this.src +' is more color img');
+                return;
+            } else if (ratio < 0.95) {
+                // console.log(this.src +' is more gray img');
+            } else {
+                // console.log(this.src +' is gray img');
+            }
+        }
+
         ctx.putImageData(imageData, 0, 0);
-        img.onload = null;
-        img.src = canvas.toDataURL('image/png');
+
+        canvas.style.width = img.width + 'px';
+        canvas.style.height = img.height + 'px';
+        img.style.display = 'none';
+        img.parentNode.insertBefore(canvas, img.nextSibling);
+        img.parentNode.removeChild(img);
+        callback();
     }
 }
 
 function MangaImage(options) {
     this.el = options.el;
     this.scale = options.scale;
-    var mangaImage = this;
+    this.imgClass = options.imgClass
+    this.auto = options.auto
+    var manga = this;
     window.onload = function() {
-        if (mangaImage.el) {
-            var images = mangaImage.el.getElementsByTagName('img');
-            if (images) {
-                images = Array.from(images);
+        if (manga.el) {
+            var images = Array.from(manga.el.getElementsByTagName('img'))
+            .filter(function(img) {
+                if (img.src.match(/\.(png|gif)$/)) return false;
+                if (manga.imgClass) {
+                    return (img.className.split(' ').indexOf(manga.imgClass) == -1 ? false : true);
+                }
+                return true;
+            });
+            if (images.length) {
                 var i = 0;
-                var worker = setInterval(function() {
-                    mangaImage.transparent(images[i++], mangaImage.scale)
-                    if (i >= images.length) clearInterval(worker);
-                }, 100);
+                var transp = function() {
+                    if (++i < images.length) manga.transparent(images[i], options, transp);
+                }
+                manga.transparent(images[0], options, transp);
             }
         }
     }
